7478e95343b2f71d1dee5a5931d04c3413fb9acb
bertyn99 <bboulikou@gmail.com>
2024-06-02 12:38:50 +0200

    feat: Add dark theme to Map component

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,26 @@
-import React from 'react';
-import Form from './components/Form';
-import Map from './components/Map';
-import './App.css';  // Assurez-vous que le chemin est correct selon la structure de vos dossiers
-import Navbar from './components/Navbar';
-import TravelDocument from './pages/TravelDocument';
-import { PDFViewer } from '@react-pdf/renderer';
+import React from "react";
+import Form from "./components/Form";
+import Map from "./components/Map";
+import "./App.css"; // Assurez-vous que le chemin est correct selon la structure de vos dossiers
+import Navbar from "./components/Navbar";
+import TravelDocument from "./pages/TravelDocument";
+import { PDFViewer } from "@react-pdf/renderer";
 
 function App() {
-  return (     
-   <div className='h-screen flex flex-col'>
-
-      <Navbar/>   
+  return (
+    <div className="h-screen flex flex-col">
+      <Navbar />
 
       <div className="flex-grow w-screen pb-10 px-10 bg-primary-bg bg-cover bg-no-repeat flex justify-center">
-      <Form />
-      <Map></Map>
-      
+        <Form />
+        <Map></Map>
+      </div>
     </div>
-    </div> 
-   /*  <PDFViewer width="100%" height="1000">
+    /*  <PDFViewer width="100%" height="1000">
        <TravelDocument />
      
       </PDFViewer> */
-    );
+  );
 }
 
 export default App;
--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
   plugins: [react()],
-  esbuild: {
-    loader: 'jsx',
-    include: /src\/.*\.[tj]sx?$/,   // Use regex to include .js and .jsx files
-    exclude: [],
-  },
 })
